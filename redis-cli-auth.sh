f128cdd2c4caa0f38cde8f613e7e3ab51359e29d
Sergiu Costache <costache.sergiu@gmail.com>
2018-11-26 00:54:45 +0200

    auth script

---
+++ redis-cli-auth.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source config.sh
+redis-cli -a $pass
--- save-redis-dump.sh
+++ save-redis-dump.sh
@@ -2,7 +2,7 @@
 
 source config.sh
 
-echo save | redis-cli
+echo save | redis-cli -a $pass
 
 mkdir -p /home/$user/redis-bkp
 tar czvf /home/$user/redis-bkp/dump.tgz -C /var/lib/redis dump.rdb
