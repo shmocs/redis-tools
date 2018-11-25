216bd98bc854a7a0af30fcb214cd9e3e24ebaef8
Sergiu Costache <costache.sergiu@gmail.com>
2018-11-26 01:03:00 +0200

    symlink comment

--- redis-cli-auth.sh
+++ redis-cli-auth.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
 source config.sh
+
+#sudo ln -s /home/$user/redis-tools/redis-cli-auth.sh /usr/bin/redis-cli-auth
+#now from anywhere you can use `redis-cli-auth`
+
 redis-cli -a $pass
