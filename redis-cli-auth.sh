63b67991b1e17482a85bf7ec3e91b79053e366db
Sergiu Costache <costache.sergiu@gmail.com>
2018-11-26 03:39:28 +0200

    redis tools changes

--- redis-cli-auth.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source ~/redis-tools/config.sh
-
-#sudo ln -s /home/$user/redis-tools/redis-cli-auth.sh /usr/bin/redis-cli-auth
-#now from anywhere you can use `redis-cli-auth`
-
-redis-cli -a $pass
--- save-redis-dump.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source config.sh
-
-echo save | redis-cli -a $pass
-
-mkdir -p /home/$user/redis-bkp
-tar czvf /home/$user/redis-bkp/dump.tgz -C /var/lib/redis dump.rdb
